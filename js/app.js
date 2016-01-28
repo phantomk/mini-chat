d77cf0211a8f2def86cb31346d09adb86a682892
phantomk <phantomk@foxmail.com>
2016-01-28 21:48:52 +0800

    基本布局

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var chat = (function () {
+    var initModule = function ( $container ) {
+        $container.html(
+            '<h1>chat</h1>'
+        );
+    };
+    
+    return {initModule: initModule};
+} ());
