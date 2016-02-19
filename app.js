1c79dd2ecc01513c7a34803bf82339e52fdb42ed
phantomk <phantomk@foxmail.com>
2016-02-19 22:55:16 +0800

    express3.x ->express4.x

--- app.js
+++ app.js
@@ -1,36 +1,50 @@
 'use strict';
 var
-  http = require( 'http' ),
   express = require( 'express' ),
+  path = require( 'path' ),
+  favicon = require( 'serve-favicon' ),
+  logger = require( 'morgan' ),
+  cookieParser = require( 'cookie-parser' ),
+  bodyParser = require( 'body-parser' ),
+  
   routes = require( './routes' ),
-  app = express(),
-  server = http.createServer( app );
+  app = express();
   
-app.configure( function () {
-  app.use( express.bodyParser() );
-  app.use( express.methodOverride() );
-  app.use( express.static( __dirname + '/public' ) );
-  app.use( app.router );
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.configure( 'development', function () {
-  app.use( express.logger() );
-  app.use( express.errorHandler({
-    dumpExceptions : true,
-    showStack      : true
-  }) );
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
-app.configure( 'production', function () {
-  app.use( express.errorHandler() );
-});
 
-routes.configRoutes( app, server );
-
-server.listen( 3000 );
-console.log(
-  'Express server listening on port %d in %s mode',
-   server.address().port, app.settings.env
-);
-  
-  
+module.exports = app;
