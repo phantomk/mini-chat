afdf33384f7e0844e9be1ecd986a08ff041175cc
phantomk <phantomk@foxmail.com>
2016-02-20 20:45:05 +0800

    修改路由

--- routes.js
+++ routes.js
@@ -1,49 +1,49 @@
 'use strict';
-var configRoutes;
+var express = require( 'express' );
+var router = express.Router();
 
-configRoutes = function ( app, server ) {
-  app.get( '/', function ( request, response ) {
-    response.redirect( '/app.html' );
+module.export = function ( app ) {
+  app.get( '/', function ( req, res ) {
+    res.redirect( '/app.html' );
   });
 
-  app.all( '/:obj_type/*?', function ( request, response, next ) {
-    response.contentType( 'json' );
+  app.all( '/:obj_type/*?', function ( req, res, next ) {
+    res.contentType( 'json' );
     next();
   });
 
-  app.get( '/:obj_type/list', function ( request, response ) {
-    response.send({ title: request.params.obj_type + ' list' });
+  app.get( '/:obj_type/list', function ( req, res ) {
+    res.send({ title: req.params.obj_type + ' list' });
   });
 
-  app.post( '/:obj_type/create', function ( request, response ) {
-    response.send({ title: request.params.obj_type + ' created' });
+  app.post( '/:obj_type/create', function ( req, res ) {
+    res.send({ title: req.params.obj_type + ' created' });
   });
 
   app.get( '/:obj_type/read/:id([0-9]+)',
-    function ( request, response ) {
-      response.send({
-        title: request.params.obj_type
-          + ' with id ' + request.params.id + ' found'
-      });
+    function ( req, res ) {
+        res.send({
+        title: req.params.obj_type
+            + ' with id ' + req.params.id + ' found'
+        });
     }
   );
 
   app.post( '/:obj_type/update/:id([0-9]+)',
-    function ( request, response ) {
-      response.send({
-        title: request.params.obj_type
-          + ' with id ' + request.params.id + ' updated'
-      });
+    function ( req, res ) {
+        res.send({
+        title: req.params.obj_type
+            + ' with id ' + req.params.id + ' updated'
+        });
     }
   );
 
   app.get( '/:obj_type/delete/:id([0-9]+)',
-    function ( request, response ) {
-      response.send({
-        title: request.params.obj_type
-          + ' with id ' + request.params.id + ' deleted'
-      });
+    function ( req, res ) {
+        res.send({
+        title: req.params.obj_type
+            + ' with id ' + req.params.id + ' deleted'
+        });
     }
   );
 };
-module.exports = { configRoutes : configRoutes };
