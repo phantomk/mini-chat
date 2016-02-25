cdd5311aa16355e46f888e4339a61b4ffb8595ff
phantomk <phantomk@foxmail.com>
2016-02-25 23:12:06 +0800

    更新socket.io

--- socket.js
+++ socket.js
@@ -1,16 +1,17 @@
 'use strict'
 
 var app = require('express')();
-var io = require('socket.io')(app);
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-server.listen(80);
+server.listen(3000);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/socket.html');
 });
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
+  socket.emit('news', { app: 'chat' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
